5ef4cb219d58f098312780214025ddd030dd3ab0
Rafa Lima <paracafa@gmail.com>
2017-11-12 00:25:39 -0300

    Colocado ViewPager Count == 3

---
+++ app/src/main/java/com/example/sudorafa/androidexerciciofragments/view/ListStudentReproFragment.java
@@ -0,0 +1,76 @@
+package com.example.sudorafa.androidexerciciofragments.view;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.example.sudorafa.androidexerciciofragments.R;
+import com.example.sudorafa.androidexerciciofragments.model.Student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnItemClick;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListStudentReproFragment extends Fragment {
+
+    @BindView(R.id.listViewStudent)
+    ListView mListViewStudent;
+
+    List<Student> mStudents;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mStudents = new ArrayList<>();
+
+        mStudents.add(new Student("Alunos Notas Inferior A 7", "paracafa@gmail.com", "81-996006450"));
+        mStudents.add(new Student("Alunos Notas Inferior A 7", "paracafa@gmail.com", "81-996006450"));
+        mStudents.add(new Student("Alunos Notas Inferior A 7", "paracafa@gmail.com", "81-996006450"));
+    }
+
+
+    public ListStudentReproFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the view for this fragment
+        View view = inflater.inflate(R.layout.fragment_list_student, container, false);
+
+        ButterKnife.bind(this, view);
+
+        mListViewStudent.setAdapter(new ArrayAdapter<Student>(getActivity(), android.R.layout.simple_list_item_1, mStudents));
+
+        return view;
+    }
+
+    @OnItemClick(R.id.listViewStudent)
+    public void itemSelectedBook(int position){
+        Student student = mStudents.get(position);
+        if(getActivity() instanceof ClickStudentListener){
+            ClickStudentListener listener = (ClickStudentListener)getActivity();
+            listener.studentClicked(student);
+        }
+    }
+
+    public interface ClickStudentListener{
+        void studentClicked(Student student);
+    }
+
+}
